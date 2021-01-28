bafd7539a20213afdeb939b55f875bf6000057f8
Jônatas Felipe Santos Gonçalves <jonatas.felipe@hotmail.com.br>
2021-01-28 00:32:04 -0300

    correção metadados

--- pages/index.js
+++ pages/index.js
@@ -38,19 +38,19 @@ export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
-        
+
         <title>Quiz - Programação</title>
         <meta name="title" content="Quiz - Programação"/>
         <meta name="description" content="Venha testar seus conhecimentos sobre programação."/>
 
         <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://metatags.io/"/>
+        <meta property="og:url" content="https://aluraquiz-base.jonatasfelipe.vercel.app/"/>
         <meta property="og:title" content="Quiz - Programação"/>
         <meta property="og:description" content="Venha testar seus conhecimentos sobre programação."/>
         <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
 
         <meta property="twitter:card" content="summary_large_image"/>
-        <meta property="twitter:url" content="https://metatags.io/"/>
+        <meta property="twitter:url" content="https://aluraquiz-base.jonatasfelipe.vercel.app/"/>
         <meta property="twitter:title" content="Quiz - Programação"/>
         <meta property="twitter:description" content="Venha testar seus conhecimentos sobre programação."/>
         <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
